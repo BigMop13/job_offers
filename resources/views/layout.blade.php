5e65a257168deb28b6357fdf14160c2f94a74598
BigMop13 <kamil.kurek12@spoko.pl>
2021-03-28 23:26:58 +0200

    Single offers and main page complete

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -63,8 +63,8 @@
 	<footer id="colophon" class="site-footer">
 	<div class="container">
 		<div class="site-info">
-			<h1 style="font-family: 'Herr Von Muellerhoff';color: #ccc;font-weight:300;text-align: center;margin-bottom:0;margin-top:0;line-height:1.4;font-size: 46px;">Moschino</h1>
-			 <a target="blank" href="https://www.wowthemes.net/">&copy; Moschino - Free HTML Template by WowThemes.net</a>
+			<h1 style="color: #ccc;font-weight:300;text-align: center;margin-bottom:0;margin-top:0;line-height:1.4;font-size: 46px;">Job IT</h1>
+			 <a target="blank" href="{{ route('welcome')}} ">Job offers from all around the world</a>
 		</div>
 	</div>
 	</footer>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,11 @@
 @extends('layout')
+@section('body')
+</br><img class="images_start" src="{{ asset('/images/first_work.png') }}"/>
+<h1>Lorem ipsum dolor sit amet</h1>
+<p >At auctor urna nunc id cursus metus. In hac habitasse platea dictumst. Non tellus orci ac auctor augue mauris augue neque. Ut tortor pretium viverra suspendisse potenti. Pretium nibh ipsum consequat nisl vel. Nunc lobortis mattis aliquam faucibus. Mollis nunc sed id semper risus in. Faucibus in ornare quam viverra orci sagittis. Eget aliquet nibh praesent tristique magna sit amet purus. Lacinia quis vel eros donec ac odio tempor. Dictum fusce ut placerat orci. Non diam phasellus vestibulum lorem sed risus ultricies tristique. Phasellus egestas tellus rutrum tellus pellentesque. Faucibus pulvinar elementum integer enim neque volutpat ac tincidunt vitae. Nunc sed augue lacus viverra. Nullam eget felis eget nunc lobortis. Donec et odio pellentesque diam volutpat commodo sed egestas egestas. Accumsan tortor posuere ac ut consequat semper viverra. Risus commodo viverra maecenas accumsan lacus vel facilisis volutpat. Odio eu feugiat pretium nibh.</p>
+
+<img class="images_start" src="{{ asset('/images/student_work.png') }}"/>
+<h1>Lorem ipsum dolor sit amet</h1>
+<p >At auctor urna nunc id cursus metus. In hac habitasse platea dictumst. Non tellus orci ac auctor augue mauris augue neque. Ut tortor pretium viverra suspendisse potenti. Pretium nibh ipsum consequat nisl vel. Nunc lobortis mattis aliquam faucibus. Mollis nunc sed id semper risus in. Faucibus in ornare quam viverra orci sagittis. Eget aliquet nibh praesent tristique magna sit amet purus. Lacinia quis vel eros donec ac odio tempor. Dictum fusce ut placerat orci. Non diam phasellus vestibulum lorem sed risus ultricies tristique. Phasellus egestas tellus rutrum tellus pellentesque. Faucibus pulvinar elementum integer enim neque volutpat ac tincidunt vitae. Nunc sed augue lacus viverra. Nullam eget felis eget nunc lobortis. Donec et odio pellentesque diam volutpat commodo sed egestas egestas. Accumsan tortor posuere ac ut consequat semper viverra. Risus commodo viverra maecenas accumsan lacus vel facilisis volutpat. Odio eu feugiat pretium nibh.</p>
+
+@endsection
