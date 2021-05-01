61e803c894af580457db82e5c1de4719bbfaae33
BigMop13 <kamil.kurek12@spoko.pl>
2021-05-02 00:17:46 +0200

    Added custom 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Job IT</title>
+<link rel='stylesheet' href='{{asset("css/woocommerce-layout.css")}}' type='text/css' media='all'/>
+<link rel='stylesheet' href='{{asset("css/woocommerce-smallscreen.css")}}' type='text/css' media='only screen and (max-width: 768px)'/>
+<link rel='stylesheet' href='{{asset("css/woocommerce.css")}}' type='text/css' media='all'/>
+<link rel='stylesheet' href='{{asset("css/font-awesome.min.css")}}' type='text/css' media='all'/>
+<link rel='stylesheet' href='{{asset("css/style.css")}}'/>
+<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Oswald:400,500,700%7CRoboto:400,500,700%7CHerr+Von+Muellerhoff:400,500,700%7CQuattrocento+Sans:400,500,700' type='text/css' media='all'/>
+<link rel='stylesheet' href='css/easy-responsive-shortcodes.css' type='text/css' media='all'/>
+<link rel="stylesheet" href="{{ mix('css/app.css') }}">
+<script
+  type="text/javascript"
+  src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.3.0/mdb.min.js"
+></script>
+<!-- Font Awesome -->
+<link
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+  rel="stylesheet"
+/>
+<!-- Google Fonts -->
+<link
+  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+  rel="stylesheet"
+/>
+<!-- MDB -->
+<link
+  href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.3.0/mdb.min.css"
+  rel="stylesheet"
+/>
+<script src="{{ mix('js/app.js') }}" defer></script>
+</head>
+<body>
+    <blockquote class="blockquote text-center">
+        <p class="mb-5">Ops, the page you are looking for were not found</p>
+        <footer class="blockquote-footer">Call support or go back to the <cite title="Source Title">welcome page</cite></footer>
+      </blockquote>
+      <center><a href="{{ route('welcome') }}"><img style="margin-top:20px;" src="http://127.0.0.1:8000/images/computer_logo.png" width="400px" height="500px"></a></center>
+
+<script src='{{asset("js/jquery.js")}}'></script>
+<script src='{{asset("js/plugins.js")}}'></script>
+<script src='{{asset("js/scripts.js")}}'></script>
+<script src='{{asset("js/masonry.pkgd.min.js")}}'></script>
+</body>
+</html>
